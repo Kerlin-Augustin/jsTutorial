e93538d13cb9bac472fb9ce067c1c3aaa1a4e5e5
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-06-24 15:16:30 -0400

    func expression

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,8 +1,16 @@
+//javascript hoists function declarations but not function expressions
 // function declaration
 
 function greet(name) {
     console.log('hello', name);
 }
 
+// function expression
+const square = function(num) {
+    return num * num;
+};
+
+// function call
 greet('Kerlin');
 greet('Tyana');
+
