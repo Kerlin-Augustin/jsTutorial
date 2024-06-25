320f945d53d553e4ec474ac989425c86715e7b4e
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-06-24 21:26:14 -0400

    this keyword

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -5,10 +5,17 @@ let user = {
     age: 29,
     email: 'kerlin@my.com',
     location: 'boston',
-    booksWritten: ['call of duty', 'programs programs programs', 'git and github'],
-    login: function() {
+    booksWritten: [{title: 'call of duty', sales: 10000}, {title: 'programs programs programs', sales: 1000000}, {title: 'git and github', sales: 10}],
+    login: () => {
         console.log('user logged in')
-    }
+    },
+    logBooks: function() {
+        console.log('I wrote these completely real no made up books')
+        this.booksWritten.forEach(book => console.log(book.title, book.sales))
+    },
+    logout(){
+        console.log('user logged out')
+    },
 }
 
 console.log(user)
@@ -22,3 +29,4 @@ console.log(user['booksWritten'])
 console.log(typeof user)
 
 user.login()
+user.logBooks()
